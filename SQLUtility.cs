b954ed34a46833328c7075022350634a27dcf73f
Mimi Levy <264mimi@gmail.com>
2023-05-17 09:14:16 +0300

    Session 18 HW #51

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -7,22 +7,38 @@ namespace CPUFrameWork
     public class SQLUtility
     {
         public static string ConnectionString = "";
+
+        public static SqlCommand GetSqlCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new(ConnectionString))
+            {
+                cmd = new(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+
+            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
+            DataTable dt = new();
+            using (SqlConnection conn = new(ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection = conn;
+                SqlDataReader dr = cmd.ExecuteReader();
+                dt.Load(dr);
+            }
+            SetAllColumnsAllowNull(dt);
+            return dt;
+        }
         public static DataTable GetDataTable(string sqlstatement)
         {
-            Debug.Print(sqlstatement);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-
-            SetAllColumnsAllowNull(dt);
-
-            return dt;
+            return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSQL(string sqlstatement)
@@ -51,6 +67,8 @@ namespace CPUFrameWork
                 c.AllowDBNull = true;
             }
         }
+
+        
         public static void DebugPrintDataTable(DataTable dt)
         {
             foreach (DataRow r in dt.Rows)

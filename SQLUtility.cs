22c83135c961f5949a5858730d055a9cddf71b7e
Mimi Levy <264mimi@gmail.com>
2023-06-01 11:51:50 +0300

    Exception Handling Session 20

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -23,15 +23,21 @@ namespace CPUFrameWork
 
         public static DataTable GetDataTable(SqlCommand cmd)
         {
-
-            Debug.Print("-----" + Environment.NewLine + cmd.CommandText);
             DataTable dt = new();
             using (SqlConnection conn = new(ConnectionString))
             {
                 conn.Open();
                 cmd.Connection = conn;
-                SqlDataReader dr = cmd.ExecuteReader();
-                dt.Load(dr);
+                try
+                {
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dt.Load(dr);
+                }
+                catch (SqlException ex)
+                {
+                    string msg = ParseConstraintMsg(ex.Message);
+                    throw new Exception(msg);
+                }
             }
             SetAllColumnsAllowNull(dt);
             return dt;
@@ -46,12 +52,49 @@ namespace CPUFrameWork
             GetDataTable(sqlstatement);
         }
 
+        private static string ParseConstraintMsg(string msg)
+        {
+            string origmsg = msg;
+            string prefix = "ck_";
+            string msgend = " ";
+            if (msg.Contains(prefix) == false)
+            {
+                if (msg.Contains("u_"))
+                {
+                    prefix = "u_";
+                    msgend = " must be unique.";
+                }
+                else if (msg.Contains("f_"))
+                {
+                    prefix = "f_";
+                }
+            }
+            if (msg.Contains(prefix))
+            {
+                msg = msg.Replace("\"", "'");
+                int pos = msg.IndexOf(prefix) + prefix.Length;
+                msg = msg.Substring(pos);
+                pos = msg.IndexOf("'");
+                if (pos == -1)
+                {
+                    msg = origmsg;
+                }
+                else
+                {
+                    msg = msg.Substring(0, pos);
+                    msg = msg.Replace("_", " ");
+                    msg = msg + msgend;
+                }
+            }
+            return msg;
+        }
+
         public static int GetFirstColumnFirstRowValue(string sql)
         {
             int n = 0;
 
             DataTable dt = GetDataTable(sql);
-            if(dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
             {
                 if (dt.Rows[0][0] != DBNull.Value)
                 {
@@ -68,7 +111,7 @@ namespace CPUFrameWork
             }
         }
 
-        
+
         public static void DebugPrintDataTable(DataTable dt)
         {
             foreach (DataRow r in dt.Rows)

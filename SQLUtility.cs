1b6c34268d22da3ee1fcfb4e487c2d8d4089fe0a
Mimi Levy <264mimi@gmail.com>
2023-09-05 10:09:54 +0300

    Finished Session 25

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -27,10 +27,19 @@ namespace CPUFrameWork
             return DoExecuteSQL(cmd, true);
         }
 
-        public static void SaveDataRow(DataRow row, string sprocname)
+        public static void SaveDataTable(DataTable dt, string sprocname)
+        {
+            var rows = dt.Select("", "", DataViewRowState.Added | DataViewRowState.ModifiedCurrent);
+            foreach (DataRow r in rows)
+            {
+                SaveDataRow(r, sprocname, false);
+            }
+            dt.AcceptChanges();
+        }
+        public static void SaveDataRow(DataRow row, string sprocname, bool acceptchanges = true)
         {
             SqlCommand cmd = GetSqlCommand(sprocname);
-            foreach(DataColumn col in row.Table.Columns)
+            foreach (DataColumn col in row.Table.Columns)
             {
                 string paramname = $"@{col.ColumnName}";
                 if (cmd.Parameters.Contains(paramname))
@@ -40,9 +49,9 @@ namespace CPUFrameWork
             }
             DoExecuteSQL(cmd, false);
 
-            foreach(SqlParameter p in cmd.Parameters)
+            foreach (SqlParameter p in cmd.Parameters)
             {
-                if(p.Direction == ParameterDirection.InputOutput)
+                if (p.Direction == ParameterDirection.InputOutput)
                 {
                     string colname = p.ParameterName.Substring(1);
                     if (row.Table.Columns.Contains(colname))
@@ -50,6 +59,10 @@ namespace CPUFrameWork
                         row[colname] = p.Value;
                     }
                 }
+            }
+            if (acceptchanges == true)
+            {
+                row.Table.AcceptChanges();
             }
         }
         private static DataTable DoExecuteSQL(SqlCommand cmd, bool loadtable)
@@ -64,8 +77,8 @@ namespace CPUFrameWork
                 {
                     SqlDataReader dr = cmd.ExecuteReader();
                     CheckReturnValue(cmd);
-                    if(loadtable == true)
-                    { 
+                    if (loadtable == true)
+                    {
                         dt.Load(dr);
                     }
                 }
@@ -74,12 +87,12 @@ namespace CPUFrameWork
                     string msg = ParseConstraintMsg(ex.Message);
                     throw new Exception(msg);
                 }
-                catch(InvalidCastException ex)
+                catch (InvalidCastException ex)
                 {
                     throw new Exception(cmd.CommandText + ": " + ex.Message, ex);
                 }
             }
-            SetAllColumnsAllowNull(dt);
+            SetAllColumnsProperties(dt);
             return dt;
         }
 
@@ -87,29 +100,29 @@ namespace CPUFrameWork
         {
             int returnvalue = 0;
             string msg = "";
-            if(cmd.CommandType == CommandType.StoredProcedure)
+            if (cmd.CommandType == CommandType.StoredProcedure)
             {
-                foreach(SqlParameter p in cmd.Parameters)
+                foreach (SqlParameter p in cmd.Parameters)
                 {
-                    if(p.Direction == ParameterDirection.ReturnValue)
+                    if (p.Direction == ParameterDirection.ReturnValue)
                     {
-                        if(p.Value != null)
+                        if (p.Value != null)
                         {
                             returnvalue = (int)p.Value;
                         }
                     }
-                    else if(p.ParameterName.ToLower() == "@message")
+                    else if (p.ParameterName.ToLower() == "@message")
                     {
-                        if(p.Value != null)
+                        if (p.Value != null)
                         {
                             msg = p.Value.ToString();
                         }
                     }
                 }
 
-                if(returnvalue == 1)
+                if (returnvalue == 1)
                 {
-                    if(msg == "")
+                    if (msg == "")
                     {
                         msg = $"{cmd.CommandText} did not do action that was requested.";
                     }
@@ -138,7 +151,7 @@ namespace CPUFrameWork
             {
                 cmd.Parameters[paramname].Value = value;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception(cmd.CommandText + ": " + ex.Message, ex);
             }
@@ -148,6 +161,7 @@ namespace CPUFrameWork
             string origmsg = msg;
             string prefix = "ck_";
             string msgend = " ";
+            string notnullprefix = "Cannot insert the value NULL into column '";
             if (msg.Contains(prefix) == false)
             {
                 if (msg.Contains("u_"))
@@ -158,6 +172,11 @@ namespace CPUFrameWork
                 else if (msg.Contains("f_"))
                 {
                     prefix = "f_";
+                }
+                else if (msg.Contains(notnullprefix))
+                {
+                    prefix = notnullprefix;
+                    msgend = " cannot be blank.";
                 }
             }
             if (msg.Contains(prefix))
@@ -176,7 +195,7 @@ namespace CPUFrameWork
                     msg = msg.Replace("_", " ");
                     msg = msg + msgend;
 
-                    if(prefix == "f_")
+                    if (prefix == "f_")
                     {
                         var words = msg.Split(" ");
                         if (words.Length > 1)
@@ -203,34 +222,71 @@ namespace CPUFrameWork
             }
             return n;
         }
-        private static void SetAllColumnsAllowNull(DataTable dt)
+        private static void SetAllColumnsProperties(DataTable dt)
         {
             foreach (DataColumn c in dt.Columns)
             {
                 c.AllowDBNull = true;
+                c.AutoIncrement = false;
             }
         }
 
+        public static int GetValueFromFirstRowAsInt(DataTable dt, string columnname)
+        {
+            int value = 0;
+            if (dt.Rows.Count > 0)
+            {
+                DataRow r = dt.Rows[0];
+                if (r[columnname] != null && r[columnname] is int)
+                {
+                    value = (int)r[columnname];
+                }
+            }
+            return value;
+        }
+
+        public static string GetValueFromFirstRowAsString(DataTable dt, string columnname)
+        {
+            string value = "";
+            if (dt.Rows.Count > 0)
+            {
+                DataRow r = dt.Rows[0];
+                if (r[columnname] != null && r[columnname] is string)
+                {
+                    value = (string)r[columnname];
+                }
+            }
+            return value;
+        }
+        public static bool TableHasChanges(DataTable dt)
+        {
+            bool b = false;
+            if (dt.GetChanges() != null)
+            {
+                b = true;
+            }
+            return b;
+        }
         public static string GetSQL(SqlCommand cmd)
         {
             string val = "";
 #if DEBUG
             StringBuilder sb = new();
 
-            if(cmd.Connection != null)
+            if (cmd.Connection != null)
             {
                 sb.AppendLine($"--{cmd.Connection.DataSource}");
                 sb.AppendLine($"use {cmd.Connection.Database}");
                 sb.AppendLine("go");
             }
 
-            if(cmd.CommandType == CommandType.StoredProcedure)
+            if (cmd.CommandType == CommandType.StoredProcedure)
             {
                 sb.AppendLine($"exec {cmd.CommandText}");
                 int paramcount = cmd.Parameters.Count - 1;
                 int paramnum = 0;
                 string comma = ",";
-                foreach(SqlParameter p in cmd.Parameters)
+                foreach (SqlParameter p in cmd.Parameters)
                 {
                     if (p.Direction != ParameterDirection.ReturnValue)
                     {
